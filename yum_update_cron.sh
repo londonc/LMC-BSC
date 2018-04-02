d172be4f907b39f9f554efce858c758a62d40212
London Crosby <london.crosby@gmail.com>
2018-04-02 15:36:38 -0400

    Update yum_update_cron.sh

--- yum_update_cron.sh
+++ yum_update_cron.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # LMC 2017
 
+# Add any packages you wish to exlude from getting auto updated. 
 XCLUDE="httpd*"
 
 echo "Running yum... "
 sudo yum update --exclude=$XCLUDE -y
+
+exit
