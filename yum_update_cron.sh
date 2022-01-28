6a3788211000a1532b8f6f9fd938c8f723e9d73b
London Crosby <london.crosby@gmail.com>
2022-01-28 03:22:40 -0500

    Update yum_update_cron.sh
    typo

--- yum_update_cron.sh
+++ yum_update_cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LMC 2017 - Schedule in cron at whatever interval you deem approprate. 
 
-# Add any packages you wish to exlude from getting auto updated. Apache is an example I've had were updates can break functionality. 
+# Add any packages you wish to exlude from getting auto updated. Apache is an example I've had where updates can break functionality.
 XCLUDE="httpd*"
 
 echo "Running yum... "
